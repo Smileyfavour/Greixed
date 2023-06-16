2c1f40780b3e1118ce00c73dec5f6e1d893cb66a
smileyfavour <smileyfavour1@gmail.com>
2023-06-16 18:09:46 +0100

    404page

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,4 @@
-
+import { useState,useEffect } from "react";
 import Link from "next/link";
 import Head from "next/head";
 import Image from "next/image";
@@ -7,7 +7,13 @@ import Image from "next/image";
 
 
 export default function PageNotFound() {
-  
+    const [screenHeight,setScreenHeight] = useState(0);
+
+
+    useEffect(()=>{
+        setScreenHeight(window.innerHeight - 60)
+    },[])
+
 
 
     return(
@@ -18,7 +24,7 @@ export default function PageNotFound() {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/greixed logo (2).png"/>
         </Head>
-        <main className={styles.container}>
+        <main className={styles.container} style={{height:`${screenHeight}px`}}>
             <Image width={400} height={400} src='/broken_plug (2).png' alt="page not found"/>
             <h3 className={styles.message}>oops! Page not Found</h3>
             <p className={styles.action}> Return to <Link className={styles.link} href='/'>Home</Link> </p>
@@ -30,7 +36,7 @@ export default function PageNotFound() {
 
 const styles={
     container:'w-full flex flex-col justify-center items-center px-16 ',
-    message:'text-lg my-3 py-3',
-    action:'text-md py-3',
+    message:'text-lg my-3',
+    action:'text-md',
     link:'text-indigo-600',
 }
